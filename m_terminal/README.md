4dddde676635bf80a280d0391b6c4036c8bd5dd5
rpi-lab-terminal <abdullahsaei@hotmail.com>
2022-11-04 16:28:50 +0100

    update run script

---
+++ m_terminal/README.md
@@ -0,0 +1,8 @@
+# POD83 Terminals
+
+This repo contains three flask apps:
+
+- server.py
+- ter1.py
+- ter2.py 
+
--- m_terminal/run.sh
+++ m_terminal/run.sh
@@ -1,3 +1,11 @@
+pkill python
+pkill chromium
+
+cd ~/r_POD83/m_terminal
 source ./venv/bin/activate
 
-python server.py &
+FLASK_APP=server.py FLASK_DEBUG=true flask run --host 0.0.0.0 --port 5500 &
+FLASK_APP=ter1.py FLASK_DEBUG=true flask run --host 0.0.0.0 --port 5551 &
+FLASK_APP=ter2.py FLASK_DEBUG=true flask run --host 0.0.0.0 --port 5552 &
+FLASK_APP=kiosk.py FLASK_DEBUG=true flask run --host 0.0.0.0 --port 5501 &
+DISPLAY=:0 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk 192.168.87.168:5501 &
