df9e5f7416abc3e270aa2085c80a0869af061d68
MartinPek <martin_pek@web.de>
2022-12-13 17:12:00 +0100

    added first event script for usb-boot, added some config parameters

--- Pi_arbiter/main.py
+++ Pi_arbiter/main.py
@@ -2,17 +2,25 @@
 
 
 import json
-import socketio
 
+import requests
+import socketio
+from time import sleep
+
+import os
 # standard Python would be python-socketIo
 
 # GPIO.add_event_detect(data.gpio, GPIO.RISING, callback=callback, bouncetime=20)
 #
 
+'''
+@TODO: expection handling
+
+'''
+
 
 try:
     import RPi.GPIO as GPIO
-
     print("Running on RPi")
 except (RuntimeError, ModuleNotFoundError):
     print("Running without GPIOs on non Pi ENV / non RPi.GPIO installed machine")
@@ -20,7 +28,6 @@ except (RuntimeError, ModuleNotFoundError):
     # GPIO.VERBOSE = False
     from GPIOEmulator.EmulatorGUI import GPIO
 
-server_ip = "http://RasPi-4-POD-T1:5500"
 
 # standard Python
 sio = socketio.Client()
@@ -30,15 +37,37 @@ sio = socketio.Client()
 # sio = socketio.AsyncClient()
 gpio_thread = None
 
+'''
+class event:
+    def __init__(self, name, gpio_trigger, gpio_out, sock):
+        self.name = name
+'''
 
-def load_settings(self):
+usb_pin = 5
+
+
+class Settings:
+    def __init__(self, event_mapping, server_add, sound_server_add):
+        # self.pin_mapping = _pin_mapping
+        self.event_mapping = event_mapping
+        self.server_add = server_add
+        self.output_GPIOs = [usb_pin]
+        self.input_GPIOs = [7]
+        self.sound_server_add = sound_server_add
+
+
+def load_settings():
     try:
         with open('config.json') as json_file:
             cfg = json.loads(json_file.read())
+            event_mapping = cfg["event_mapping"]
+            terminal_server_add = cfg["terminal_server_add"]
+            sound_server_add = cfg["sound_server_add"]
     except ValueError as e:
         print('failure to read config.json')
         print(e)
         exit()
+    return Settings(event_mapping, terminal_server_add, sound_server_add)
 
 
 @sio.event
@@ -47,15 +76,50 @@ def connect():
 
 
 def usb_boot():
-    sio.emit("usb_boot", "boot")
+    # sio.emit("usb_boot", "boot")
+    GPIO.output(usb_pin, GPIO.LOW)
+    payload = {
+        "ip": "192.168.178.172",
+        "group_id": 2
+    }
+    ret = requests.post(settings.sound_server_add, payload)
+    print(ret)
+
+
+def setup_gpios():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    for pin in settings.input_GPIOs:
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    for pin in settings.output_GPIOs:
+        GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
+
+
+def scan_for_usb():
+    return True
 
 
 def main():
     while True:
-        usb_boot()
+        if scan_for_usb():
+            usb_boot()
+            sleep(20)
 
 
 if __name__ == '__main__':
-    sio.connect(server_ip)
-    # socketio.run(app, host='0.0.0.0', port=80, debug=False, allow_unsafe_werkzeug=True)
+    settings = load_settings()
+    setup_gpios()
+    # usb_boot()
+    # exit()
+    connected = False
+    while not connected:
+        try:
+            sio.connect(settings.server_add)
+            connected = True
+        except socketio.exceptions.ConnectionError as exc:
+            print(f'Caught exception socket.error : {exc}')
+            sleep(1)
+
     main()
--- Pi_arbiter/notes.md
+++ Pi_arbiter/notes.md
@@ -1,14 +1,10 @@
 * bit of an example of what we need to do
 
-https://flask-socketio.readthedocs.io/en/latest/getting_started.html#sending-messages
+import subprocess
+rv3 = subprocess.call("mountpoint -q /mnt/usbdrive", shell=True)
 
-https://github.com/miguelgrinberg/Flask-SocketIO/issues/409
-
-https://github.com/miguelgrinberg/Flask-SocketIO/issues/1298
-
-https://socket.io/docs/v3/emitting-events/
-
-https://stackoverflow.com/questions/52788198/how-to-emit-message-from-python-server-to-javascript-client-in-python-socketio
-
-https://python-socketio.readthedocs.io/en/latest/client.html
-
+incoming and outgoing from and to sio
+coming from USB 
+coming from and going to GPIO
+going to requests to pod-ITX
+https://stackoverflow.com/questions/47495206/how-to-detect-a-new-usb-device-is-connected-on-python
