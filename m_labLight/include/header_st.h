108e13f4509841fd66a765c0c0687f1c3b1334c4
MartinPek <martin_pek@web.de>
2023-02-17 13:46:13 +0100

    m_lablight: added clrs

--- m_labLight/include/header_st.h
+++ m_labLight/include/header_st.h
@@ -17,6 +17,8 @@ enum IO {
     lightNormalBright_pin,      // black  
     lightRed_pin,          // Green
     lightRedBright_pin,          // Green
+    lightBlue_pin,          // Green
+    lightBlueBright_pin,          // Green
 };
 
 const int lightOff = 1 << lightOff_pin;
@@ -24,9 +26,11 @@ const int lightNormal = 1 << lightNormal_pin;
 const int lightNormalBright = 1 << lightNormalBright_pin;
 const int lightRed = 1 << lightRed_pin; 
 const int lightRedBright = 1 << lightRedBright_pin; 
+const int lightBlue = 1 << lightBlue_pin; 
+const int lightBlueBright = 1 << lightBlueBright_pin; 
 
 #define outputCnt 0
-#define inputCnt 5
+#define inputCnt 7
 
 static constexpr int clrLight[3] = {255,200,120};
 
@@ -37,6 +41,8 @@ int intputArray[inputCnt] = {
     lightNormalBright_pin,      // black  
     lightRed_pin,          // Green
     lightRedBright_pin,          // Green  
+    lightBlue_pin,          // Green
+    lightBlueBright_pin,          // Green
 };
 
 int outputArray[outputCnt] = {
