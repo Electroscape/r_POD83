68a4a6b9343e0378c57038ab916f6d1ab743cb8f
MartinPek <martin_pek@web.de>
2023-03-16 16:47:20 +0100

    Pi_tvs: added ending videos as passable param

--- pi_tvs/videoTrigger.py
+++ pi_tvs/videoTrigger.py
@@ -7,13 +7,16 @@ from time import sleep
 
 sock = SocketClient('raspi-4-pod-arbiter', 12345)
 video_dict = {
-    "usb_boot": "welcome.mp4"
+    "usb_boot": "welcome.mp4",
+    "end_rachel_announce": "Rachel_Ending.mp4",
+    "end_david_announce": "Rachel_Elancell_Ending.mp4"
 }
 
 
 def start_backgroound():
-    return Popen(["cvlc", "black_screen.jpg", "--no-embedded-video", "--fullscreen", "--no-video-title", "--video-wallpaper",
-           "--loop"])
+    return Popen(["cvlc", "black_screen.jpg", "--no-embedded-video", "--fullscreen", "--no-video-title",
+                  "--video-wallpaper", "--loop"])
+
 
 def main():
     video_proc = None
@@ -32,13 +35,13 @@ def main():
             video_name = video_dict[msg]
             background.kill()
             print(f"playing video {video_name}")
-            video_proc = Popen(['cvlc', video_name,
-               "--no-embedded-video", "--fullscreen", '--no-video-title', '--video-on-top'])
-            sleep(67)
-            background = start_backgroound()
+            video_proc = Popen(['cvlc', video_name, "--no-embedded-video", "--fullscreen",
+                                '--no-video-title', '--video-on-top'])
+            if msg == list(video_dict.keys())[0]:
+                sleep(67)
+                background = start_backgroound()
         except KeyError:
             print(f"unknown key received {msg}")
-
 
 
 main()
