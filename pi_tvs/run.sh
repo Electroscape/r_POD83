99d31678b145cb3058a887f87729a75795c751a0
MartinPek <martin_pek@web.de>
2023-03-17 12:13:15 +0100

    pi_tvs: updated run.sh

--- pi_tvs/run.sh
+++ pi_tvs/run.sh
@@ -1,8 +1,11 @@
-sudo pkill vlc
-sudo pkill fbi
+pkill vlc
+# sudo pkill fbi
+pkill python
+
+# cd to this script dir
+# fixes activate venv from crontab 
+cd "${0%/*}"
 
 # sudo fbi -T 1 -noverbose -a black_screen.jpg
 export DISPLAY=:0.0
-
-
 python3 videoTrigger.py
