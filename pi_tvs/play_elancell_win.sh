75ec4233830e5d240be64386b2c7c9f9c63d912c
MartinPek <martin_pek@web.de>
2023-03-18 17:51:18 +0100

    Pi_TVs: updated shellscipts to play multiple videos in sequence

--- pi_tvs/play_elancell_win.sh
+++
@@ -1,9 +0,0 @@
-export DISPLAY=:0.0
-pkill vlc
-
-cvlc --fullscreen --no-video-title --video-on-top --play-and-exit --quiet elancell_win.mp4 &
-cvlc --no-embedded-video --fullscreen --no-video-title --video-wallpaper --quiet --loop black_screen.jpg &
-echo "video played, back to blank screen, bye"
-
-
-
--- pi_tvs/play_rachel.sh
+++ pi_tvs/play_rachel.sh
@@ -2,6 +2,7 @@ export DISPLAY=:0.0
 pkill vlc
 
 cvlc --fullscreen --no-video-title --video-on-top --play-and-exit --quiet Rachel_Ending.mp4 &
+cvlc --fullscreen --no-video-title --video-on-top --play-and-exit --quiet security_breach.mp4 &
 cvlc --no-embedded-video --fullscreen --no-video-title --video-wallpaper --quiet --loop black_screen.jpg &
 
 
