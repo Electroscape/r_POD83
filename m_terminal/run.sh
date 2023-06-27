a816e4c531427024a33a10c1d02ac719ef35656f
ASmac <abdullah@teamescape.de>
2023-06-27 16:19:17 +0200

    fix audio not playing in kiosk mode and crontab

--- m_terminal/run.sh
+++ m_terminal/run.sh
@@ -4,17 +4,17 @@ pkill flask
 pkill chromium
 
 # cd to this script dir
-# fixes activate venv from crontab 
-cd "${0%/*}"
+# fixes activate venv from crontab
 # shellcheck disable=SC2164
+cd "${0%/*}"
 source venv/bin/activate
 
-# comment or uncomment to run server 
+# comment or uncomment to run server
 FLASK_APP=server.py flask run --host 0.0.0.0 --port 5500 --no-debugger --no-reload &
 sleep 10 &&
 
-# comment or uncomment to run terminal 1
-FLASK_APP=ter1.py flask run --host 0.0.0.0 --port 5551 --no-debugger --no-reload &
+  # comment or uncomment to run terminal 1
+  FLASK_APP=ter1.py flask run --host 0.0.0.0 --port 5551 --no-debugger --no-reload &
 
 # comment or uncomment to run terminal 2
 FLASK_APP=ter2.py flask run --host 0.0.0.0 --port 5552 --no-debugger --no-reload &
@@ -24,6 +24,10 @@ FLASK_APP=kiosk.py flask run --host 0.0.0.0 --port 5501 --no-debugger --no-reloa
 
 sleep 10 &&
 
+  # set environment variable to enable sound
+  XDG_RUNTIME_DIR=/run/user/$(id -u)
+export XDG_RUNTIME_DIR
+
 # modify the IP address you want to display
 # comment or uncomment to run the browser on the webpage you choose
-DISPLAY=:0 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk 0.0.0.0:5501 &
+DISPLAY=:0 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --autoplay-policy=no-user-gesture-required --disable-features=AudioServiceSandbox --kiosk 0.0.0.0:5501 &
