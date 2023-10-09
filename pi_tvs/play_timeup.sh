28114b3912e9c0a52dc985e765264da691ab8af1
Martin Pek <martin.pek@web.de>
2023-10-09 12:34:41 +0200

    changed name of file in play_timeup.sh to a suitable preset

--- pi_tvs/play_timeup.sh
+++ pi_tvs/play_timeup.sh
@@ -1,5 +1,5 @@
 export DISPLAY=:0.0
 pkill vlc
 
-cvlc --fullscreen --no-video-title --video-on-top --play-and-exit --quiet --gain=1.0 Rachel_Ending.mp4 &
+cvlc --fullscreen --no-video-title --video-on-top --play-and-exit --quiet --gain=1.0 timeup.mp4 &
 cvlc --no-embedded-video --fullscreen --no-video-title --video-wallpaper --quiet --loop black_screen.jpg &
