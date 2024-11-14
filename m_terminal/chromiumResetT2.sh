2e24ea1f411350eac78428f81f590393d9fec56c
Martin Pek <martin.pek@web.de>
2024-11-14 12:56:00 +0100

    split chromium reset function for T1 and T2

---
+++ m_terminal/chromiumResetT2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pkill chromium-browse
+
+
+# set environment variable to enable sound
+XDG_RUNTIME_DIR=/run/user/$(id -u)
+export XDG_RUNTIME_DIR
+
+# modify the IP address you want to display
+# comment or uncomment to run the browser on the webpage you choose
+DISPLAY=:0 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --autoplay-policy=no-user-gesture-required --disable-features=AudioServiceSandbox --kiosk 0.0.0.0:5552 &
